a2153f5b7d694cdc82dde2b6407e00522a985864
UneSo6 <unconfigured@null.spigotmc.org>
2023-03-19 22:46:28 +0100

    Commented Equipe Match

--- Belote/src/Equipe.java
+++ Belote/src/Equipe.java
@@ -1,5 +1,12 @@
 
 public 	class Equipe{
+	/**
+	 * Constructeur d'une équipe
+	 * @param _id identifiant unique de l'équipe
+	 * @param _num numéro d'affichage de l'équipe
+	 * @param _eq1 équipier 1
+	 * @param _eq2 équipier 2
+	 */
 	Equipe( int _id, int _num, String _eq1, String _eq2){
 		id = _id;
 		num = _num;
--- Belote/src/MatchM.java
+++ Belote/src/MatchM.java
@@ -2,6 +2,16 @@
 		public int idmatch,eq1,eq2,score1,score2,num_tour;
 		public boolean termine;
 		 
+		/**
+		 * Constructeur du Match
+		 * @param _idmatch identifiant unique du match
+		 * @param _e1 équipe 1
+		 * @param _e2 équipe 2
+		 * @param _score1 score de l'équipe 1
+		 * @param _score2 scode de l'équipe 2
+		 * @param _num_tour tour actuel
+		 * @param _termine la partie est-elle finie
+		 */
 		public MatchM(int _idmatch,int _e1,int _e2,int _score1, int _score2, int _num_tour, boolean _termine){
 			idmatch = _idmatch;
 			eq1     = _e1;
