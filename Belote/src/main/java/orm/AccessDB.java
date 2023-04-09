54ef8562727c3f069e2140c52e8f570d564f7d9a
Anxton <anxton4@gmail.com>
2023-04-09 22:12:04 +0200

    fix main accessDB

--- Belote/src/main/java/orm/AccessDB.java
+++ Belote/src/main/java/orm/AccessDB.java
@@ -5,15 +5,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 
 public class AccessDB {
     
+    private static AccessDB instance = null;
+
     private Connection connection;
     private Statement statement;
+
+    public static AccessDB getInstance() {
+        if (instance == null) {
+            instance = new AccessDB();
+        }
+        return instance;
+    }
+
+    private AccessDB() { }
 
     public void initializeDatabase() {
         try {
@@ -39,48 +50,56 @@ public class AccessDB {
         } 
     }
 
-    private void importSQL(Connection connection, File sqlFile) throws SQLException, IOException {
-        // implémentation de la méthode importSQL
+    private void importSQL(Connection conn, File in) throws SQLException, IOException {
+        Scanner s = new Scanner(in);
+        s.useDelimiter("(;(\r)?\n)|(--\n)");
+        Statement st = null;
+        try
+        {
+            st = conn.createStatement();
+            while (s.hasNext())
+            {
+                String line = s.next();
+                if (line.startsWith("/*!") && line.endsWith("*/"))
+                {
+                    int i = line.indexOf(' ');
+                    line = line.substring(i + 1, line.length() - " */".length());
+                }
+                
+                if (line.trim().length() > 0)
+                {
+                    //System.out.println("Req:" + line);
+                    st.execute(line);
+                }
+            }
+        }
+        finally
+        {
+            if (st != null) st.close();
+        }
     }
 
     public Statement getStatement() {
         return statement;
     }
-}
-    
 
 
+    // public static int chercherTournoi(String nomTournoi) throws SQLException {
+    //     // Connection conn = /* obtenir la connexion à la base de données */;
+    //     PreparedStatement ps = conn.prepareStatement("SELECT * FROM tournois WHERE nom_tournoi = ?");
+    //     ps.setString(1, nomTournoi);
+    //     ResultSet rs = ps.executeQuery();
 
-}
+    //     if (!rs.next()) {
+    //         return -1; // le tournoi n'a pas été trouvé
+    //     }
 
-// faire une méthode pour chaque select dans le code
+    //     int statut = rs.getInt("statut");
+    //     int idTournoi = rs.getInt("id_tournoi");
 
+    //     rs.close();
+    //     ps.close();
 
-
-
-
-
-
-
-
-
-
-public static int chercherTournoi(String nomTournoi) throws SQLException {
-    Connection conn = /* obtenir la connexion à la base de données */;
-    PreparedStatement ps = conn.prepareStatement("SELECT * FROM tournois WHERE nom_tournoi = ?");
-    ps.setString(1, nomTournoi);
-    ResultSet rs = ps.executeQuery();
-
-    if (!rs.next()) {
-        return -1; // le tournoi n'a pas été trouvé
-    }
-
-    int statut = rs.getInt("statut");
-    int idTournoi = rs.getInt("id_tournoi");
-
-    rs.close();
-    ps.close();
-
-    return idTournoi;
-}
+    //     return idTournoi;
+    // }
 }
